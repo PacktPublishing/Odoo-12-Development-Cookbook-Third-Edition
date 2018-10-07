a9dc0d0a05345ee5c0eaf0641d5cc46c4c271caf
Parth Gajjar <parthegajjar@gmail.com>
2018-10-07 12:10:32 +0530

    Added Example Code For Chapter04

---
+++ Chapter04/r5_menu_and_views/my_library/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",  # Module title
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """Long description""",  # You can also rst format
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base'],
+    # This data files will be loaded at the installation (commented becaues file is not added in this example)
+    'data': [
+        'views/library_book.xml'
+    ],
+    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
