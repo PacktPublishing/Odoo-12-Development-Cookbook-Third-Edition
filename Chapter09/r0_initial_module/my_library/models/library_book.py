dad1c25adb55e3e5b1de8609c0c4d923c9e71a38
Parth Gajjar <parthegajjar@gmail.com>
2018-10-30 23:08:02 +0530

    [CH9] Initial module for Chapter 09

---
+++ Chapter09/r0_initial_module/my_library/models/library_book.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+
+    name = fields.Char('Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many('res.partner', string='Authors')
+    state = fields.Selection(
+        [('available', 'Available'),
+         ('borrowed', 'Borrowed'),
+         ('lost', 'Lost')],
+        'State', default="available")
+    cost_price = fields.Float('Book Cost')
+    category_id = fields.Many2one('library.book.category')
+
+    def make_available(self):
+        self.ensure_one()
+        self.state = 'available'
+
+    def make_borrowed(self):
+        self.ensure_one()
+        self.state = 'borrowed'
+
+    def make_lost(self):
+        self.ensure_one()
+        self.state = 'lost'
+
+
