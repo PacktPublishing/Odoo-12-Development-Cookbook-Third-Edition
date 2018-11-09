e1ee5e62be0e5d296f5d69c3457802f326e58b53
Parth Gajjar <parthegajjar@gmail.com>
2018-11-10 00:01:21 +0530

    [CH9] Added more recipes for Chapter 09

---
+++ Chapter09/r9_init_hooks/my_library/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from . import models
+from . import controllers
+from . import wizard
+from odoo import api, fields, SUPERUSER_ID
+
+def add_book_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    book_data1 = {'name': 'Book 1', 'release_date': fields.Date.today()}
+    book_data2 = {'name': 'Book 2', 'release_date': fields.Date.today()}
+    env['library.book'].create([book_data1, book_data2])
