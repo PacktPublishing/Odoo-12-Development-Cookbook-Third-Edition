0294fd565f7f86ffddb7a24072d858d016df376d
Parth Gajjar <parthegajjar@gmail.com>
2018-11-12 09:18:55 +0530

    [CH10] Added recipes for Chapter 010

---
+++ Chapter10/r7_list_view/my_module/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Module",  # Module title
+    'description': """Long description""",  # You can also rst format
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base'],
+    'data': [
+        'views/my_contacts.xml',
+    ],
+}
