251d72456d91153f35011429a3f7fbcc41073182
Parth Gajjar <parthegajjar@gmail.com>
2018-11-18 01:53:48 +0530

    [CH10] Added more recipes for Chapter 10

---
+++ Chapter10/r17_graph_pivot_view/my_project/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Project",  # Module title
+    'description': """Long description""",  # You can also rst format
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Project',
+    'version': '12.0.1',
+    'depends': ['project'],
+    'data': [
+        'views/my_tasks.xml',
+    ],
+}
