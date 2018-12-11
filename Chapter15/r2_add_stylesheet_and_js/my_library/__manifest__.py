de09490c76749f539d0c69ce7c013e0aa50e964b
Parth Gajjar <parthegajjar@gmail.com>
2018-12-12 01:20:34 +0530

    [CH15] Added recipes for Chapter 15

---
+++ Chapter15/r2_add_stylesheet_and_js/my_library/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",  # Module title
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """Long description""",  # You can also rst format
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base', 'website'],
+    # This data files will be loaded at the installation (commented becaues file is not added in this example)
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/templates.xml'
+    ],
+    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
+    # 'demo': [
+    #     'demo.xml'
+    # ],
+}
