87c375d093c6b32a82dba6b7111ff5fe338a6765
Parth Gajjar <parthegajjar@gmail.com>
2018-12-23 23:00:02 +0530

    [CH16] Added recipes for Chapter 16

---
+++ Chapter16/r4_views/my_library/models/ir_action_act_window.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class ActWindowView(models.Model):
+    _inherit = 'ir.actions.act_window.view'
+
+    view_mode = fields.Selection(selection_add=[('m2m_group', 'M2m group')])
