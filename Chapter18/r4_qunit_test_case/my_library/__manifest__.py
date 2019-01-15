d6842c5680c7609cfdfbfecf74202231785cc6f2
Parth Gajjar <parthegajjar@gmail.com>
2019-01-16 00:19:33 +0530

    [CH18] Added recipes for Chapter 18

---
+++ Chapter18/r4_qunit_test_case/my_library/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",  # Module title
+    'summary': "Manage books easily",  # Module subtitle phrase
+    'description': """Long description""",  # You can also rst format
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base', 'web'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/templates.xml'
+    ],
+}
